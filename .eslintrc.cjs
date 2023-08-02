25bc9c3fca3a26f4c776ac20840f5604883d3038
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-02 12:32:18 +0800

    config(lint): add preferred rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,41 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	"root": true,
+	"env": { "browser": true, "es2020": true },
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:react-hooks/recommended",
+	],
+	"ignorePatterns": ["dist"],
+	"parser": "@typescript-eslint/parser",
+	"plugins": ["react-refresh"],
+	"rules": {
+		"max-lines": "warn",
+		"indent": [
+			"error",
+			"tab",
+			{
+				"SwitchCase": 1
+			}
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"double"
+		],
+		"quote-props": ["warn", "always"],
+		"require-await": "error",
+		"semi": [
+			"error",
+			"always"
+		],
+		"no-unused-vars": "off",
+		"react-refresh/only-export-components": [
+			"warn",
+			{ "allowConstantExport": true },
+		],
+	},
+};
