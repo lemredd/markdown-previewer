e173fd32eff21d658a47a96cbec97a395f789c6e
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-03 11:02:00 +0800

    feat: set `DEFAULT_CONTENT_VALUE`

--- src/App.tsx
+++ src/App.tsx
@@ -15,8 +15,35 @@ import reactLogo from "./assets/react.svg";
 marked.use(mangle() as MarkedExtension);
 marked.use(gfmHeadingId() as MarkedExtension);
 
+const DEFAULT_CONTENT_VALUE = `
+# Welcome to Markdown Previewer!
+## Created by [Lem Redd](https://www.github.com/lemredd)
+
+Try editing the editor to see the **magic** happen!
+Here are some sample markdown elements you can use:
+
+### Inline code
+\`code\`
+
+### Code block
+\`\`\`javascript
+	const msg = "Hello Javascript!";
+\`\`\`
+
+### List items
+- Item #1
+- Item #2
+- Item #3
+
+### Blockquotes
+> "I code, therefore I am." - Rene DesCodes
+
+### Image
+![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+`;
+
 function App(): ReactElement {
-	const [content, set_content] = useState<string>("");
+	const [content, set_content] = useState<string>(DEFAULT_CONTENT_VALUE);
 	const handle_change = (event: ChangeEvent<HTMLTextAreaElement>): void => set_content(event.target.value);
 	const preview = useRef<HTMLDivElement>(null);
 
