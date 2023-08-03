6c9d18a9d654472fd8a191a6673a4cfdc28e48d5
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-03 11:16:19 +0800

    fix: recognize line breaks

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ import reactLogo from "./assets/react.svg";
 
 marked.use(mangle() as MarkedExtension);
 marked.use(gfmHeadingId() as MarkedExtension);
+marked.setOptions({ "breaks": true });
 
 const DEFAULT_CONTENT_VALUE = `
 # Welcome to Markdown Previewer!
