f5e0f153edef3a52a4a9382b5ad2f68ad465e2d8
Lem Redd <lmoa.jhdp@gmail.com>
2023-08-08 14:53:54 +0800

    chore(component): make `#maximizer` checkbox

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -8,6 +8,7 @@ interface Props {
 export default function Editor({ content, on_change }: Props): ReactElement {
 	return (
 		<div className="editor-container">
+			<input type="checkbox" id="maximizer" hidden />
 			<textarea value={content} id="editor" onChange={on_change}></textarea>
 			<button className="toggle-expand-btn material-symbols-outlined">chevron_right</button>
 		</div>
